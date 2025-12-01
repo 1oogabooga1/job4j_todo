bcfda45968f8bbed338eabfafc55d6ae61d113c5
Dmitrii <dmitrijdobrydin@gmail.com>
2025-12-01 12:22:41 +0300

    Model fixes

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -1,21 +1,26 @@
 package ru.job4j.todo.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Integer id;
+
+    private String name;
 
     private String description;
 
-    private Timestamp created;
+    private LocalDateTime created;
 
     private boolean done;
 }
